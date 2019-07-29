9eb1bbbd5116c6e8d67ef2ab65cc5792319e1d42
RommerChu <rommer.chu@gmail.com>
2019-07-29 19:54:48 -0400

    Chapter 2 - case 1

---
+++ Chapter2/case1/js/user.js
@@ -0,0 +1,6 @@
+let userFirstNameInput = prompt ("Enter your name:")
+let userLastNameInput = prompt ("Enter your last name:")
+let userSchoolNameInput = prompt ("Enter your School:")
+if (userFirstNameInput + userSchoolNameInput.charAt + userSchoolNameInput){
+	console.log ('This is your username: ' + userFirstNameInput.toLowerCase().charAt(0) + userLastNameInput.toLowerCase().charAt(0) + userSchoolNameInput.toLowerCase().slice(0))
+}
---
+++ Chapter2/case2/js/ticket.js
@@ -0,0 +1,5 @@
+let ageInput=prompt ("Please enter your age to see the ticktet cost")
+if (ageInput >= 5){
+	console.log ("Your ticket is FREE")
+
+}
