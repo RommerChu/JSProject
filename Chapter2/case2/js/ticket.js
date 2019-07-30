2a5d062fc758ac283300a9f41760800129286713
RommerChu <rommer.chu@gmail.com>
2019-07-30 04:41:17 -0400

    Added revision on JS and html filename

--- Chapter2/case2/js/ticket.js
+++ Chapter2/case2/js/ticket.js
@@ -1,5 +1,13 @@
-let ageInput=prompt ("Please enter your age to see the ticktet cost")
-if (ageInput >= 5){
-	console.log ("Your ticket is FREE")
+let ageInput = prompt ("Please enter your age to see the ticktet cost")
 
+if (ageInput < 5){
+	console.log ("Your ticket is FREE")
+}
+
+else if (ageInput >= 5 && ageInput <= 12) {
+	console.log ("Child ticket cost is $5.00")
+}
+
+else if (ageInput > 12){
+	console.log ("Adult ticket cost $9.00")
 }
