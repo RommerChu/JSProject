6fcbc0a535abbf12e669fdfc1ef85aff8404c78f
RommerChu <rommer.chu@gmail.com>
2019-07-30 05:43:59 -0400

    modified user js to add split instead of slice

--- Chapter2/case1/js/user.js
+++ Chapter2/case1/js/user.js
@@ -1,6 +1,7 @@
 let userFirstNameInput = prompt ("Enter your name:")
 let userLastNameInput = prompt ("Enter your last name:")
 let userSchoolNameInput = prompt ("Enter your School:")
+let word = userSchoolNameInput.split(' ')
 if (userFirstNameInput + userSchoolNameInput + userSchoolNameInput){
-	console.log ('This is your username: ' + userFirstNameInput.toLowerCase().charAt(0) + userLastNameInput.toLowerCase().charAt(0) + userSchoolNameInput.toLowerCase().slice(0))
+	console.log ('This is your username: ' + userFirstNameInput.toLowerCase().charAt(0) + userLastNameInput.toLowerCase().charAt(0) + word[0])
 }
