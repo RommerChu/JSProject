95feb4059a0d6c633adb78bce4bda998a695dbee
RommerChu <rommer.chu@gmail.com>
2019-08-01 18:17:54 -0400

    Update expression.js
    modified the script

--- Chapter2/case3/js/expression.js
+++ Chapter2/case3/js/expression.js
@@ -4,44 +4,38 @@ let z=5
 
 function myFunction1 (){
 if ((x > 5) && (x < 10) == !(x <=5) || !(x >= 10)){
-	document.getElementById('btn1').innerHTML = "true"
-	console.log("true")
+	alert("true")
+	
 }
 else {
-	document.getElementById('btn1').innerHTML = "false"
-	console.log("false")
+	alert("false")
+	
 	}
 }
 
 function myFunction2 (){
 if (![(x > y) && (y < z)] == !(x > y) || !(y < z)){
-	document.getElementById('btn2').innerHTML = "true"
-	console.log("true")
+	alert("true")
 }
 else {
-	document.getElementById('btn2').innerHTML = "false"
-	console.log("false")
+	alert("false")
 	}
 }
 	
 function myFunction3 (){
 if ((x == y) || (x > y) == (x == y) && (x < y)){
-	document.getElementById('btn3').innerHTML = "true"
-	console.log("true")
+	alert("true")
 }
 else {
-	document.getElementById('btn3').innerHTML = "false"
-	console.log("false")
+	alert("false")
 	}
 }	
 
 function myFunction4 (){
 if (![(z < x) || ( z < y)] == !(z < x) && !(z < y)){
-	document.getElementById('btn4').innerHTML = "true"
-	console.log("true")
+	alert("true")
 }
 else {
-	document.getElementById('btn4').innerHTML = "false"
-	console.log("false")
+	alert("false")
 	}
 }	
