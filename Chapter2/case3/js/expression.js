f112c7d6870cea0ae8741d784a1ad69dab8526f8
RommerChu <rommer.chu@gmail.com>
2019-08-01 16:47:02 -0400

    update the expression.js

--- Chapter2/case3/js/expression.js
+++ Chapter2/case3/js/expression.js
@@ -5,35 +5,43 @@ let z=5
 function myFunction1 (){
 if ((x > 5) && (x < 10) == !(x <=5) || !(x >= 10)){
 	document.getElementById('btn1').innerHTML = "true"
+	console.log("true")
 }
 else {
 	document.getElementById('btn1').innerHTML = "false"
+	console.log("false")
 	}
 }
 
 function myFunction2 (){
 if (![(x > y) && (y < z)] == !(x > y) || !(y < z)){
 	document.getElementById('btn2').innerHTML = "true"
+	console.log("true")
 }
 else {
 	document.getElementById('btn2').innerHTML = "false"
+	console.log("false")
 	}
 }
 	
 function myFunction3 (){
 if ((x == y) || (x > y) == (x == y) && (x < y)){
 	document.getElementById('btn3').innerHTML = "true"
+	console.log("true")
 }
 else {
 	document.getElementById('btn3').innerHTML = "false"
+	console.log("false")
 	}
 }	
 
 function myFunction4 (){
 if (![(z < x) || ( z < y)] == !(z < x) && !(z < y)){
 	document.getElementById('btn4').innerHTML = "true"
+	console.log("true")
 }
 else {
 	document.getElementById('btn4').innerHTML = "false"
+	console.log("false")
 	}
 }	
