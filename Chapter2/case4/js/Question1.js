7e7db24ffee7b630b4c171112197c0e7cb0c2ef7
Harpreet <sonianandra30@gmail.com>
2019-08-12 19:50:31 -0400

    fixed it

--- Chapter2/case4/js/Question1.js
+++ Chapter2/case4/js/Question1.js
@@ -1,7 +1,7 @@
 let userEnter = prompt ("Enter your age")
 if (userEnter >= 18){
-	console.log ("You can vote")
+	alert("You can vote")
 }
-else if(userEnter < 18){
-	console.log ("you are too young to vote")
+else {
+	 console.log ("you are too young to vote")
 }
