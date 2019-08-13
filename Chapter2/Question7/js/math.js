028fe9e421f1277b401d0b83be3006848621dbf4
Harpreet <sonianandra30@gmail.com>
2019-08-13 19:48:55 -0400

    chapter2 question7

---
+++ Chapter2/Question7/js/math.js
@@ -0,0 +1,69 @@
+function question1(){
+	let user1 = prompt ("Enter your answer")
+	if (user1 == 5 + 9) {
+		alert("correct")
+	}
+	else {
+		alert("incorrect")
+	}
+}
+
+function answer1() {
+	alert("Correct answer is" + " " +(user1 = 5+9))
+}
+
+function question2(){
+	let user1 = prompt ("Enter your answer")
+	if (user1 == 4 * 6) {
+		alert("correct")
+	}
+	else {
+		alert("incorrect")
+	}
+}
+
+function answer2() {
+	alert("Correct answer is" + " " +(user1 = 4*6))
+}
+
+function question3(){
+	let user1 = prompt ("Enter your answer")
+	if (user1 == 25 - 14) {
+		alert("correct")
+	}
+	else {
+		alert("incorrect")
+	}
+}
+
+function answer3() {
+	alert("Correct answer is" + " " +(user1 = 25 - 14))
+}
+
+function question4(){
+	let user1 = prompt ("Enter your answer")
+	if (user1 == 48/3) {
+		alert("correct")
+	}
+	else {
+		alert("incorrect")
+	}
+}
+
+function answer4() {
+	alert("Correct answer is" + " " +(user1 = 48/3))
+}
+
+function question5(){
+	let user1 = prompt ("Enter your answer")
+	if (user1 == 26%6) {
+		alert("correct")
+	}
+	else {
+		alert("incorrect")
+	}
+}
+
+function answer5() {
+	alert("Correct answer is" + " " +(user1 = 26%6))
+}
--- Chapter2/case5/Question4.js
+++
@@ -1,7 +0,0 @@
-let age 18
-let age = prompt("Enter your age");
-	if (age >=18){
-		alert("you can vote")
-	}
-	else
-		console.log("You are too young to vote")
