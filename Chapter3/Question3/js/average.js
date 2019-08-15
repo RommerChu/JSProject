2474b3d9278d32bc7961fb4336fee5d3a47721c8
Harpreet <sonianandra30@gmail.com>
2019-08-14 21:06:51 -0400

    chapter3Question3

---
+++ Chapter3/Question3/js/average.js
@@ -0,0 +1,21 @@
+let userEnter= prompt ("Enter your grade")
+if (userEnter < 60){
+	console.log ("F")
+}
+else if(userEnter >= 60 && userEnter <= 69.5){
+		console.log("D")
+	}
+
+else if(userEnter >=69.6 && userEnter <= 79.5){
+			console.log ("C")
+		}
+		
+else if (userEnter >=79.5 && userEnter <= 89.5){
+			console.log ("B")
+		}
+
+else if (userEnter > 89.5){
+			console.log ("A")
+		}
+
+
