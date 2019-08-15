5d90247049f88b045821124ef9cac7509402b6b9
RommerChu <rommer.chu@gmail.com>
2019-08-15 19:04:16 -0400

    Chapter 4, question 1

---
+++ Chapter4/Question1/js/countdown.js
@@ -0,0 +1,7 @@
+function blast1() {
+	let count = 10
+	for (i=0; i<=10; i--){
+				
+	}
+	
+}console.log(count)
---
+++ Chapter4/Question1/js/timer.js
@@ -0,0 +1,15 @@
+let timer = 1000
+let sec = 0
+function countdown () {
+	sec = (timer%20)
+	if (timer == 979) {
+		document.getElementById("time").innerHTML = "BLAST OFF!"
+	}
+	else{
+		document.getElementById("time").innerHTML = sec.toString()
+	timer--
+	setTimeout(function() {
+		countdown();
+	}, 1000)
+	}
+}
