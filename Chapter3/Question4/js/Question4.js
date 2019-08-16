952853ba241dbb5b175d3bcac1ddecebc47dee9f
Harpreet <sonianandra30@gmail.com>
2019-08-15 21:30:52 -0400

    chapter3_Question4

---
+++ Chapter3/Question4/js/Question4.js
@@ -0,0 +1,25 @@
+
+function color()
+	{
+		let color = prompt("Enter a background color:"," ");
+		switch (color)
+		{
+			case "orange":
+				document.body.bgColor="orange";
+				break;
+		
+			case "yellow":
+				document.body.bgColor="yellow";
+				break;
+			case "pink":
+				document.body.bgColor="pink";
+				break;
+			case "blue":
+				document.body.bgColor="blue";
+				break;
+			case "brown":
+				document.body.bgColor="brown";
+				break;
+		}
+	}
+	
---
+++ Chapter3/harpreet/js/color.js
@@ -0,0 +1,7 @@
+function color()
+{
+ alert("fun called");
+ let color_name = document.getElementById("txtcolor").value;
+ 
+	document.body.style.backgroundColor = color_name;
+}
