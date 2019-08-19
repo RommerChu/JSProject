965b5d2065cf05f4d9103483b0e8d2669111fa1f
RommerChu <rommer.chu@gmail.com>
2019-08-19 14:45:12 -0400

    Modified/revised chapter5, question 1

--- Chapter5/Question1/js/round.js
+++ Chapter5/Question1/js/round.js
@@ -1,16 +1,12 @@
-function roundIt(){
-	let z = document.getElementById("orig").value;
-	//while (isNaN(userInput) || (userInput<0)){alert("Please enter a number")
-	
-	let x = 0
-	let y = 0
-		
-	if (z > x){
-		x = parseInt(Math.ceil("orig").toFixed(2));
-		document.getElementById("rndUp").innerHTML = x;
-	}
-	else if (z < y){
-		y = parseInt(Math.floor("orig").toFixed(2));
-		document.getElementById("rndDn").innerHTML = y;
-	}
-}
+let userInput = prompt ("Please enter a number with decimal")
+document.write("<table width='50%' align='center'><tr><td colspan='2'><h4>This is the original value input:</h4>" + userInput + "</td></tr>")
+
+let x = Math.ceil(userInput)
+let y = Math.floor(userInput)
+console.log(x)
+console.log(y)
+
+document.write("<tr><td colspan='2'>" + "</td></tr>")
+document.write("<tr><td colspan='2'>" + "Round up number is: " + x + "</td></tr>")
+document.write("<tr><td colspan='2'>" + "</td></tr>")
+document.write("<tr><td colspan='2'>" + "Round down number is: " + y + "</td></tr>")
