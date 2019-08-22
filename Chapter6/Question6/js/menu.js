964852ac6ade13c7b66dbc5b9506fd4be940442c
Harpreet <sonianandra30@gmail.com>
2019-08-22 18:16:22 -0400

    Chapter6,Question6

---
+++ Chapter6/Question6/js/menu.js
@@ -0,0 +1,9 @@
+function CusName(Name){
+	let LuncheonMenu = document.getElementById(Name).value
+	document.getElementById('Name').innerHTML=LuncheonMenu;
+}
+
+function CusPhone (Cell) {
+	let Phone = document.getElementById(Cell).value
+	document.getElementById('phone').innerHTML=Phone;
+}
